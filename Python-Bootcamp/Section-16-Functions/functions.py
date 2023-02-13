6216bf2adb696f73f0d2d7bbc0435fbe0526f2d2
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-13 15:16:37 +0200

    Updated Functions included positional and keyword args

--- Python-Bootcamp/Section-16-Functions/functions.py
+++ Python-Bootcamp/Section-16-Functions/functions.py
@@ -4,8 +4,43 @@ def my_funct():
     x = 10
     print(x ** 10)
 
+
 # functions wont run unless called
 my_funct()  # <-- function called and will run
 
 # docstrings
+print(len('Python is coool'))  # hover for builtin help
 
+
+def say_hi(name):
+    """This Function says hello your name"""  # <-- documentation string for func explanation.
+    print(f'Hello {name}!!!!')
+
+
+say_hi('Shay')  # <--hover over for docstr
+help(say_hi)
+print(say_hi.__doc__)
+
+
+# positonal args
+# key word args ignore order of positional
+# def args
+# *args
+# **kwargs
+
+def diffr(a, b):
+    return a - b
+
+
+x = diffr(1, 5)
+print(x)
+
+
+def func1(x, y, z):  # <-- params local to function
+    print(f'X: {x}')
+    print(f'Y: {y}')
+    print(f'Z: {z}')
+
+
+# func1(1, 555)  # <-- arguments
+func1(y=1, z=50, x=1023)  # keyword args
