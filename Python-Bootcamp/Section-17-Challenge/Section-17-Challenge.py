db2e492704185bf30c3e9d3d0109056875442a5f
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-13 20:24:11 +0200

    Challenge 1 done

--- Python-Bootcamp/Section-17-Challenge/Section-17-Challenge.py
+++ Python-Bootcamp/Section-17-Challenge/Section-17-Challenge.py
@@ -1,5 +1,13 @@
 # Challenge 1
+def uni_lst(lst):
+    ord_lst = []
+    for i in lst:
+        if i not in ord_lst:
+            ord_lst.append(i)
+    return ord_lst
 
+
+print(uni_lst([1, 2, 3, 3, 3, 3, 4, 5, 1, 3, 5, 5, 5]))
 # =====================================================================================================================
 # Challenge 1
 
