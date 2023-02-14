d0f27c74ae20d1717dccc2c28579ae2b9f8549a0
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-14 11:20:57 +0200

    Challenge 8 completed

--- Python-Bootcamp/Section-19-Challenge/Section-19-Challenge.py
+++ Python-Bootcamp/Section-19-Challenge/Section-19-Challenge.py
@@ -76,6 +76,19 @@ with open('txtfiles/banking.txt') as f:
     print(balance)
 # =====================================================================================================================
 # Challenge 8
+with open('txtfiles/f1.txt') as f:
+    file1 = f.read().splitlines()
+
+with open('txtfiles/f2.txt') as f:
+    file2 = f.read().splitlines()
+
+file = list(zip(file1, file2))
+
+i = 0
+for item in file:
+    i += 1
+    if item[0] != item[1]:
+        print(f'file1 ({i}): {item[0]}, file2 ({i}): {item[1]}')
 # =====================================================================================================================
 # Challenge 1
 # =====================================================================================================================
