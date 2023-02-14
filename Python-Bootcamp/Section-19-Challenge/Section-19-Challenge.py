89a98c9db035bc30de57bc7661ec1642719e4696
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-14 10:55:48 +0200

    Challenge 3 completed

--- Python-Bootcamp/Section-19-Challenge/Section-19-Challenge.py
+++ Python-Bootcamp/Section-19-Challenge/Section-19-Challenge.py
@@ -12,7 +12,12 @@ with open('txtfiles/sample_file.txt') as f:
     content_str = f'\n{content}'
     print(content_str)
 # =====================================================================================================================
-# Challenge 1
+# Challenge 3
+with open('txtfiles/file.txt') as f:
+    content_lst = f.readlines()
+remove_space = [l for l in content_lst if l.strip() != '']
+with open('txtfiles/no_blanks.txt', 'a') as f:
+    f.write(''.join(remove_space))
 # =====================================================================================================================
 # Challenge 1
 # =====================================================================================================================
