240b3777777c5184521c73e9cf56bd6f4bf2e4a7
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-15 16:04:03 +0200

    Section-25 builtin modules updated

--- Python-Bootcamp/Section-25-BuiltinModules/BuiltinModules.py
+++ Python-Bootcamp/Section-25-BuiltinModules/BuiltinModules.py
@@ -26,4 +26,32 @@ x = max(os.listdir(), key=os.path.getatime)
 
 # os.mkdir('dir1')
 # os.mkdris('dir1\\dir2')
-
+# os.__file__  # => '/usr/lib/python3.7/os.py'
+#
+# cwd = os.getcwd()  # => returns the current working directory
+# print(cwd)
+#
+# ## changes the current working directory, OS specific
+# os.chdir('/home/user1/python')  # Linux specific
+# os.chdir('C:\\Users')  # Windows specific
+#
+# os.path.isfile('/etc/passwd')  # returns True if argument is a file
+# os.path.isdir('/tmp')  # returns True if argument is a directory
+#
+# ## Splits the extension from a pathname, returns a tuple
+# name, extention = os.path.splitext('/var/log/kern.log')  # name is 'kern', extention is '.log'
+#
+# ## Listing a directory
+# os.listdir('.')  # returns the entries in the current working directory as a list
+# os.listdir('C:\\Users')  # returns the entries in the directory as a list
+#
+# os.path.getmtime('/etc/passwd')  # => 1547047769.9049096, returns file's last modification time as a timestamp
+# os.path.getsize('/etc/passwd')  # => 2691, returns the size of a file
+#
+# os.mkdir('PATH/dir')  # creates a new directory in PATH, os specific
+#
+# ## Creates a new directory called dir1 in Windows (permissions required)
+# os.mkdir('C:\\dir1')
+#
+# ## Creates a new directory called dir1 in Linux
+# os.mkdir('~/dir1')
