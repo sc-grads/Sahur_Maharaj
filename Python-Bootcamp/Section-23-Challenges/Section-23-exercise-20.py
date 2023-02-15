0eae0dfac1c12bc75301ad4fa1eb886067c0784c
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-15 13:19:41 +0200

    Added section 23 ex 20

---
+++ Python-Bootcamp/Section-23-Challenges/Section-23-exercise-20.py
@@ -0,0 +1,5 @@
+countries = ['USA', 'UK', 'France', 'Romania', 'France', 'Germany', 'USA', 'Canada', 'India', 'UK']
+
+# YOUR CODE STARTS HERE
+countries = sorted(set(countries))
+print(list(countries))
