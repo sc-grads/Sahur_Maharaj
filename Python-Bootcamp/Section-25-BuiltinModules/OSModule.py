2a33d916cdc0a46ae0a72ddf6334ffd3c164167c
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-15 16:39:17 +0200

    Added os and subprocess

--- Python-Bootcamp/Section-25-BuiltinModules/OSModule.py
+++ Python-Bootcamp/Section-25-BuiltinModules/OSModule.py
@@ -1,5 +1,13 @@
-import os
+import os, subprocess
+
 out = os.popen('arp -a').read()
 print(out)
 
-os.system('type nul > abc.txt')
+# os.system('type nul > abc.txt')  <-- creates a file
+
+cmd = ['arp', '-a']
+subprocess.check_output(cmd)
+cmd_ping = ['ping', '-n', '300', '8.8.8.8']
+outpt = subprocess.check_output(cmd_ping)
+print(str(outpt.decode()))
+
