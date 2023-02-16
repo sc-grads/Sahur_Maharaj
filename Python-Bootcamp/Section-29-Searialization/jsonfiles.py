daf479aaed3a34b0d2799c212f5804b61d7db832
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-16 10:31:09 +0200

    added json

---
+++ Python-Bootcamp/Section-29-Searialization/jsonfiles.py
@@ -0,0 +1,31 @@
+import json
+
+# Declaring a dictionary
+friends = {"Dan": (20, "London", 13242252), "Maria": [25, "Madrid", 34232424]}
+# Serializing the dictionary to a text file called `friends.json`
+with open('friends.json', 'wt') as f:
+    json.dump(friends, f, indent=4)
+# Serializing the dictionary to a JSON encoded string
+json_string = json.dumps(friends, indent=4)
+print(json_string)
+# Deserializing from file into a Python Object
+with open('friends.json') as f:
+    obj = json.load(f)
+    print(type(obj))  # => dict
+    print(obj)  # => friends = {"Dan": (20, "London", 13242252), "Maria":[25, "Madrid", 34232424]}
+# Loading a JSON encoded string intro a Python Object
+json_string = """{
+    "Dan": [
+        20,
+        "London",
+        13242252
+    ],
+    "Maria": [
+        25,
+        "Madrid",
+        34232424
+    ]
+}"""
+obj = json.loads(json_string)
+print(type(obj))
+print(obj)
