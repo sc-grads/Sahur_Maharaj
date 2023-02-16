f5f0c0250614547611c9bbf0b188c3506c1b3a55
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-16 10:11:05 +0200

    Challenge 1 complete

--- Python-Bootcamp/Section-28-Challenge/Section-28-Challenges.py
+++ Python-Bootcamp/Section-28-Challenge/Section-28-Challenges.py
@@ -1,4 +1,19 @@
 # Challenge 1
+import csv
+people = [
+['Dan', 34, 'Bucharest'],
+['Andrei',21, 'London'],
+['Maria', 45, 'Paris']
+]
+
+with open('people.csv', 'w') as f:
+    writer = csv.writer(f)
+    for i in people:
+        writer.writerow(i)
+with open('people.csv') as f:
+    read = csv.reader(f)
+    for r in read:
+        print(r)
 # =====================================================================================================================
 # Challenge 2
 # =====================================================================================================================
