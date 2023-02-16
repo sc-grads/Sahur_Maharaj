ebe56a06909083c61f34c9872721d4acd742d141
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-16 10:13:30 +0200

    Challenge 2 complete

--- Python-Bootcamp/Section-28-Challenge/Section-28-Challenges.py
+++ Python-Bootcamp/Section-28-Challenge/Section-28-Challenges.py
@@ -16,4 +16,12 @@ with open('people.csv') as f:
         print(r)
 # =====================================================================================================================
 # Challenge 2
+with open('people_new.csv', 'w') as f:
+    writer = csv.writer(f, delimiter=':')
+    for w in people:
+        writer.writerow(w)
+with open('people_new.csv') as f:
+    read = csv.reader(f, delimiter=':')
+    for r in read:
+        print(r)
 # =====================================================================================================================
