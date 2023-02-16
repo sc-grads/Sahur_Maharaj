94af19dfb6242a87eadaa0ebb359840a911f80e1
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-16 10:32:18 +0200

    added json

--- Python-Bootcamp/Section-29-Searialization/jsonfiles.py
+++ Python-Bootcamp/Section-29-Searialization/jsonfiles.py
@@ -1,6 +1,5 @@
 import json
 
-# Declaring a dictionary
 friends = {"Dan": (20, "London", 13242252), "Maria": [25, "Madrid", 34232424]}
 # Serializing the dictionary to a text file called `friends.json`
 with open('friends.json', 'wt') as f:
