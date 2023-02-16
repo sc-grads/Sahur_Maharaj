26025a05eb274ca274df590e01f9ae178de225cb
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-16 09:15:12 +0200

    Updated Decorators in python.

--- Python-Bootcamp/Section-26-Decorators/Decorators.py
+++ Python-Bootcamp/Section-26-Decorators/Decorators.py
@@ -56,3 +56,43 @@ print(xx)  # => 1
 
 print(func1())  # => 2
 print(func1())  # => 3
+
+
+# Decorators
+user = {'usrname': '123', 'lvl': 'admin'}
+
+def only_admin(fn):
+    def wrapper_only_admin(*args, **kwargs):
+        if user['lvl'] == 'admin':
+            return fn(*args, **kwargs)
+        else:
+            raise PermissionError('Error')
+    return wrapper_only_admin
+
+def rm_file(f=None):
+    import os, os.path
+    if os.path.isfile(f):
+        os.remove(f)
+        print(f"removed: {f}")
+    else:
+        print('FNE')
+
+remove_file = only_admin(rm_file)
+remove_file('a.txt')
+
+
+
+def permission(func):
+    if user['lvl'] == 'admin':
+        return func
+    else:
+        return None
+
+def show_pass():
+    return 'rgezssshg'
+
+fn = permission(show_pass)
+if fn != None:
+    print(fn())
+else:
+    print('Cannot run function')
