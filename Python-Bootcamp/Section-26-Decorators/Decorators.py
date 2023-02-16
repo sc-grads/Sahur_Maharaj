318335ef1f65af394245c56459943d98132a15f2
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-02-16 08:58:55 +0200

    Updated Decorators in python.

---
+++ Python-Bootcamp/Section-26-Decorators/Decorators.py
@@ -0,0 +1,58 @@
+# High order function
+import math
+
+
+def sum(n, fn):
+    res = 0
+    for i in range(1, n + 1):
+        res += fn(n)
+
+    return res
+
+
+def sqare(x):
+    return x ** 2
+
+
+print(sum(3, sqare))
+
+res = sum(3, math.sqrt)
+print(res)
+
+
+# functions creturning a function
+def cpute(msg):
+    if msg == 'square':
+        return sqare
+    else:
+        return math.sqrt
+
+
+func = cpute('square')
+print(func(10))
+
+# functions can be nested
+def outer():
+    msg = 'Python'
+    x = 0
+
+    # defining an inner function
+    def inner():
+        # nonlocal is used to declare that a variable inside a nested function
+        # is not local to it (it lies in the outer enclosing function).
+        nonlocal x  # this is `x` from outer function
+        x += 1
+        print(f'{msg} is really cool!')  # `msg` and `x` are free variables
+        return x
+
+    return inner
+
+
+func1 = outer()
+print(type(func1))  # => <class 'function'>
+
+xx = func1()
+print(xx)  # => 1
+
+print(func1())  # => 2
+print(func1())  # => 3
