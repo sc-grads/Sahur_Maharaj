8238407863d1758936810725fa08de09e62d0077
Sahur-Maharaj <sahur.maharaj@sambeconsulting.com>
2023-03-06 12:49:26 +0200

    Added Timesheet-Project folder structure

---
+++ Timesheet-Project/Database-Scripts/MasterScript.sql
@@ -0,0 +1,20 @@
+-- Creating user for application
+-- Creating login for application
+
+-- Creating application database
+-- Creating tables in the database
+
+-- Adding demo data in to database
+
+-- Triggers
+
+-- Indexes
+
+-- Procedures
+
+
+-- DANGER ZONE --
+-- Deleting and recreating the database
+-- Deleting login
+-- Deleting user
+-- Deleting tables
