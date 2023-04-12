795f146c4e88a9fa7d52dece4e9868d9b41dcf7f
Sahur-Maharaj <Sahur.maharaj@sambeconsulting.com>
2023-04-12 23:46:15 +0200

    reskind page

--- Projects/Timesheet-Project/Frontend/chronosync/src/app/app.module.ts
+++ Projects/Timesheet-Project/Frontend/chronosync/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AdminPannelComponent} from "./components/admin-pannel/admin-pannel.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminPannelComponent
   ],
   imports: [
     BrowserModule
