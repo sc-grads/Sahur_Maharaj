8132ec5f618226805b7248fda65d37b9e07b4dca
Sahur Maharaj <124353059+Sahur-Maharaj@users.noreply.github.com>
2023-05-08 08:21:52 +0200

    Updated Full Repo

--- Projects/Timesheet-Project/Frontend/chronosync/src/app/app.module.ts
+++ Projects/Timesheet-Project/Frontend/chronosync/src/app/app.module.ts
@@ -2,30 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
-import { Routes, RouterModule } from '@angular/router';
-import { UserPanelComponent } from './components/user-panel/user-panel.component';
-
-const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'user/:user', component: UserPanelComponent},
-
-
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    UserPanelComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot(routes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
